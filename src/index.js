596288594d9893e809460cdc1efea26ac3559e90
Julian Lloyd <julianlloyd@gmail.com>
2019-02-14 04:33:15 -0800

    Implement Acorn Walk base extension

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-export default function() {}
+function jsxWalk(walk) {
+  const base = walk.base
+  const { ExpressionStatement, Identifier, SpreadElement } = base
+
+  base.JSXExpressionContainer = ExpressionStatement
+  base.JSXSpreadChild = ExpressionStatement
+
+  base.JSXClosingFragment = Identifier
+  base.JSXEmptyExpression = Identifier
+  base.JSXIdentifier = Identifier
+  base.JSXOpeningFragment = Identifier
+  base.JSXText = Identifier
+
+  base.JSXSpreadAttribute = SpreadElement
+
+  base.JSXAttribute = function(node, state, callback) {
+    callback(node.name, state)
+    if (node.value) callback(node.value, state)
+  }
+
+  base.JSXMemberExpression = (node, state, callback) => {
+    callback(node.object, state)
+    callback(node.property, state)
+  }
+
+  base.JSXNamespacedName = (node, state, callback) => {
+    callback(node.namespace, state)
+    callback(node.name, state)
+  }
+
+  base.JSXOpeningElement = function(node, state, callback) {
+    callback(node.name, state)
+    for (let i = 0; i < node.attributes.length; ++i) {
+      callback(node.attributes[i], state)
+    }
+  }
+
+  base.JSXClosingElement = function(node, state, callback) {
+    callback(node.name, state)
+  }
+
+  base.JSXElement = function(node, state, callback) {
+    callback(node.openingElement, state)
+    for (let i = 0; i < node.children.length; ++i) {
+      callback(node.children[i], state)
+    }
+    if (node.closingElement) callback(node.closingElement, state)
+  }
+
+  base.JSXFragment = function(node, state, callback) {
+    callback(node.openingFragment, state)
+    for (let i = 0; i < node.children.length; ++i) {
+      callback(node.children[i], state)
+    }
+    callback(node.closingFragment, state)
+  }
+
+  return walk
+}
+
+export default jsxWalk
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,79 @@
 import { assert } from 'chai'
+import jsxWalk from '../src'
 
-describe('JSX Walk', () => {
-  it('Smoke test', () => {
-    assert.equal(1, 1)
+let output
+const mockWalk = {
+  base: {
+    ExpressionStatement: true,
+    Identifier: true,
+    SpreadElement: true,
+  },
+}
+
+describe('JSX support for Acorn Walk', () => {
+  before(() => (output = jsxWalk(mockWalk)))
+
+  describe('Bindings', () => {
+    it('Extends walk base with JSXExpressionContainer', () => {
+      assert.isDefined(output.base.JSXExpressionContainer)
+    })
+
+    it('Extends walk base with JSXSpreadChild', () => {
+      assert.isDefined(output.base.JSXSpreadChild)
+    })
+
+    it('Extends walk base with JSXClosingFragment', () => {
+      assert.isDefined(output.base.JSXClosingFragment)
+    })
+
+    it('Extends walk base with JSXEmptyExpression', () => {
+      assert.isDefined(output.base.JSXEmptyExpression)
+    })
+
+    it('Extends walk base with JSXIdentifier', () => {
+      assert.isDefined(output.base.JSXIdentifier)
+    })
+
+    it('Extends walk base with JSXOpeningFragment', () => {
+      assert.isDefined(output.base.JSXOpeningFragment)
+    })
+
+    it('Extends walk base with JSXText', () => {
+      assert.isDefined(output.base.JSXText)
+    })
+
+    it('Extends walk base with JSXSpreadAttribute', () => {
+      assert.isDefined(output.base.JSXSpreadAttribute)
+    })
+  })
+
+  describe('JSX Nodes', () => {
+    it('Extends walk base with JSXAttribute', () => {
+      assert.isDefined(output.base.JSXAttribute)
+    })
+
+    it('Extends walk base with JSXMemberExpression', () => {
+      assert.isDefined(output.base.JSXMemberExpression)
+    })
+
+    it('Extends walk base with JSXNamespacedName', () => {
+      assert.isDefined(output.base.JSXNamespacedName)
+    })
+
+    it('Extends walk base with JSXOpeningElement', () => {
+      assert.isDefined(output.base.JSXOpeningElement)
+    })
+
+    it('Extends walk base with JSXClosingElement', () => {
+      assert.isDefined(output.base.JSXClosingElement)
+    })
+
+    it('Extends walk base with JSXElement', () => {
+      assert.isDefined(output.base.JSXElement)
+    })
+
+    it('Extends walk base with JSXFragment', () => {
+      assert.isDefined(output.base.JSXFragment)
+    })
   })
 })
