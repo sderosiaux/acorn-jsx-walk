21a772de398f6be7f3b73c13dc6680475f7e159a
Julian Lloyd <julianlloyd@gmail.com>
2019-02-14 01:44:06 -0800

    Strip embedded walk implementation

--- test/index.js
+++ test/index.js
@@ -1,18 +1,5 @@
 import test from 'ava';
 
-import walk, { base } from '../lib/index.js';
-
-test('simple test', t => {
-  const source = 'const a = 2; const f = u => k => { const m = <div>Hey</div>; return m; }';
-  const names = [];
-
-  walk(source, {
-    VariableDeclaration: (node) => names.push(node.declarations.map(n => n.id.name))
-  });
-
-  t.deepEqual(names, [
-    ['a'],
-    ['m'],
-    ['f']
-  ]);
+test('smoke test', t => {
+  t.deepEqual(1, 1);
 });
